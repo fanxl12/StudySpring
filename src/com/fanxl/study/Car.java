6e733f6be7178f5fa703a9866aab6c45471b0a41
fanxl12 <1964645988@qq.com>
2018-09-15 16:10:37 +0800

    bean注入以及构造方法注入;

---
+++ src/com/fanxl/study/Car.java
@@ -0,0 +1,41 @@
+package com.fanxl.study;
+
+/**
+ * @description
+ * @author: fanxl
+ * @date: 2018/9/15 0015 15:57
+ */
+public class Car {
+
+    private String brand;
+
+    private String corp;
+
+    private double price;
+
+    private int maxSpeed;
+
+    public Car(String brand, String corp, double price) {
+        super();
+        this.brand = brand;
+        this.corp = corp;
+        this.price = price;
+    }
+
+    public Car(String brand, String corp, int maxSpeed) {
+        super();
+        this.brand = brand;
+        this.corp = corp;
+        this.maxSpeed = maxSpeed;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", corp='" + corp + '\'' +
+                ", price=" + price +
+                ", maxSpeed=" + maxSpeed +
+                '}';
+    }
+}
--- src/com/fanxl/study/Main.java
+++ src/com/fanxl/study/Main.java
@@ -12,9 +12,27 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // 1.创建Spring的IOC容器对象
         ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // 2.从IOC容器中获取Bean实例 name是xml配置bean的id
         Person person = (Person) ctx.getBean("person");
+
+        // 另外一种获取实例的方式，邀请xml中只配置了一个bean， 如果有多个会报错
+        Person p = ctx.getBean(Person.class);
+
+        // 3.调用方法
         person.sayName();
+        p.sayName();
+
+        Car car = (Car) ctx.getBean("car");
+        System.out.println(car);
+
+        Car car1 = (Car) ctx.getBean("car1");
+        System.out.println(car1);
+
+        Car car2 = (Car) ctx.getBean("car2");
+        System.out.println(car2);
 
     }
 
