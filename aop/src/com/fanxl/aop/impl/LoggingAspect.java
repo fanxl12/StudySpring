9ef5dd52c036ea3b638a88e1fb002f4ce846c62a
fanxl12 <1964645988@qq.com>
2018-09-28 19:27:33 +0800

    切面的其他的几种方式;

--- aop/src/com/fanxl/aop/impl/LoggingAspect.java
+++ aop/src/com/fanxl/aop/impl/LoggingAspect.java
@@ -1,8 +1,8 @@
 package com.fanxl.aop.impl;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
@@ -21,9 +21,48 @@ public class LoggingAspect {
     public void beforeMethod(JoinPoint joinPoint) {
         String methodName = joinPoint.getSignature().getName();
         List<Object> args = Arrays.asList(joinPoint.getArgs());
-
         System.out.println("The method " + methodName + " begins with " + args);
     }
 
+    // 后置通知，在目标方法执行后执行(无论是否发生异常，都会执行)
+    @After("execution(* com.fanxl.aop.impl.*.*(int, int))")
+    public void afterMethod(JoinPoint joinPoint) {
+        String methodName = joinPoint.getSignature().getName();
+        System.out.println("The method " + methodName + " end" );
+    }
+
+    // 返回通知，方法正常执行完返回
+    @AfterReturning(value = "execution(* com.fanxl.aop.impl.*.*(int, int))",
+        returning = "result")
+    public void afterReturning(JoinPoint joinPoint, Object result) {
+        String methodName = joinPoint.getSignature().getName();
+        System.out.println("The method " + methodName + " end with " + result);
+    }
+
+    // 在目标方法出现异常的时候执行
+    // 可以访问到异常对象，且可以指定在出现特定异常时执行通知代码
+    @AfterThrowing(value = "execution(* com.fanxl.aop.impl.*.*(int, int))",
+            throwing = "ex")
+    public void afterThrowing(JoinPoint joinPoint, NullPointerException ex) {
+        String methodName = joinPoint.getSignature().getName();
+        System.out.println("The method " + methodName + " occurs exception:" + ex);
+    }
+
+    /**
+     * 环绕通知需要携带ProceedingJoinPoint类型参数
+     * 环绕通知类似于动态代理全过程 ProceedingJoinPoint的参数key决定是否执行目标方法
+     * 且环绕通知必须有返回值，返回值即为目标方法的返回值
+     * @param pjd
+     */
+    @Around("execution(* com.fanxl.aop.impl.*.*(int, int))")
+    public Object aroundMethod(ProceedingJoinPoint pjd) {
+        Object result = null;
+        try {
+            result = pjd.proceed();
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+        return result;
+    }
 
 }
