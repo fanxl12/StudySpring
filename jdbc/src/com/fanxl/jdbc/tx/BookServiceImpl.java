8c0e95a668653651e7ba0dd8cbace8cbf376b99f
fanxl12 <1964645988@qq.com>
2018-09-30 18:36:54 +0800

    事务的其他属性介绍;

--- jdbc/src/com/fanxl/jdbc/tx/BookServiceImpl.java
+++ jdbc/src/com/fanxl/jdbc/tx/BookServiceImpl.java
@@ -2,6 +2,7 @@ package com.fanxl.jdbc.tx;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,10 +23,16 @@ public class BookServiceImpl implements BookShopService {
      * 使用propagation指定事务的传播行为，即当前的事务方法被另外一个事务方法调用时
      * 如果使用事务， 默认取值REQUIRED 使用上一个事务方法的事务
      * REQUIRES_NEW 使用自己的事务，调用的方法的事务被挂起
+     * isolation 指定事务的隔离级别，最常用的取值为READ_COMMITTED 读已提交
+     * readOnly 是否是只读事务，表示一个事务只读数据不更新数据，设置只读事务可以提高性能
+     * timeout 指定强制回滚之前事务可以占用的时间, 如果业务在指定时间内未完成就会强制回滚
      * @param name
      * @param isbn
      */
-    @Transactional(rollbackFor = Exception.class, propagation = Propagation.REQUIRES_NEW)
+    @Transactional(rollbackFor = Exception.class, propagation = Propagation.REQUIRES_NEW,
+        isolation = Isolation.READ_COMMITTED,
+        readOnly = false,
+        timeout = 1)
     @Override
     public void buyBook(String name, String isbn) {
 
